330bfd6b4c9f52255fc695b27a3262ec5a3b74de
devitfro <alinabogdanovaaaaa@gmail.com>
2025-01-07 18:27:27 +0200

    update

--- hw09.12/hw09.12/1dArray.cs
+++ hw09.12/hw09.12/1dArray.cs
@@ -1,82 +1,82 @@
-﻿//namespace hw1;
+﻿namespace hw1;
 
-//class _1dArray
-//{
-//    static void Main()
-//    {
-//        Console.Write("Введите число от 0 до 1 000 000: ");
-//        string userInput = Console.ReadLine();
+class _1dArray
+{
+    static void Main()
+    {
+        Console.Write("Введите число от 0 до 1 000 000: ");
+        string userInput = Console.ReadLine();
 
-//        bool success = int.TryParse(userInput, out var number);
+        bool success = int.TryParse(userInput, out var number);
 
-//        if (!success || number < 0 || number >= 1000000)
-//        {
-//            Console.WriteLine("Ошибка: введите число в диапазоне от 0 до 1 000 000.");
-//            return;
-//        }
+        if (!success || number < 0 || number >= 1000000)
+        {
+            Console.WriteLine("Ошибка: введите число в диапазоне от 0 до 1 000 000.");
+            return;
+        }
 
-//        Console.WriteLine("Ваше число - " + Result(number));
-//    }
+        Console.WriteLine("Ваше число - " + Result(number));
+    }
 
-//    static string Result(int number)
-//    {
-//        if (number == 0) return "ноль";
-//        if (number == 1000000) return "один миллион";
+    static string Result(int number)
+    {
+        if (number == 0) return "ноль";
+        if (number == 1000000) return "один миллион";
 
-//        string result = "";
+        string result = "";
 
-//        if (number >= 1000)
-//        {
-//            int thousands = number / 1000;
-//            result += Thousands(thousands) + " тысяча ";
-//            number %= 1000;
-//        }
+        if (number >= 1000)
+        {
+            int thousands = number / 1000;
+            result += Thousands(thousands) + " тысяча ";
+            number %= 1000;
+        }
 
-//        result += Hundreds(number);
+        result += Hundreds(number);
 
-//        return result;
-//    }
+        return result;
+    }
 
-//    static string Thousands(int number)
-//    {
-//        if (number == 1) return "одна";
-//        if (number == 2) return "две";
+    static string Thousands(int number)
+    {
+        if (number == 1) return "одна";
+        if (number == 2) return "две";
 
-//        return Hundreds(number);
-//    }
+        return Hundreds(number);
+    }
 
-//    static string Hundreds(int number)
-//    {
-//        string[] hundreds = { "", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот" };
-//        string[] tens = { "", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" };
-//        string[] teens = { "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
-//        string[] ones = { "", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+    static string Hundreds(int number)
+    {
+        string[] hundreds = { "", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот" };
+        string[] tens = { "", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" };
+        string[] teens = { "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
+        string[] ones = { "", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
 
-//        string result = "";
+        string result = "";
 
-//        if (number >= 100)
-//        {
-//            result += hundreds[number / 100] + " ";
-//            number %= 100;
-//        }
+        if (number >= 100)
+        {
+            result += hundreds[number / 100] + " ";
+            number %= 100;
+        }
 
-//        if (number >= 10 && number < 20)
-//        {
-//            result += teens[number - 10] + " ";
-//        }
-//        else
-//        {
-//            if (number >= 20)
-//            {
-//                result += tens[number / 10] + " ";
-//                number %= 10;
-//            }
-//            if (number > 0)
-//            {
-//                result += ones[number] + " ";
-//            }
-//        }
+        if (number >= 10 && number < 20)
+        {
+            result += teens[number - 10] + " ";
+        }
+        else
+        {
+            if (number >= 20)
+            {
+                result += tens[number / 10] + " ";
+                number %= 10;
+            }
+            if (number > 0)
+            {
+                result += ones[number] + " ";
+            }
+        }
 
-//        return result;
-//    }
-//}
+        return result;
+    }
+}
--- hw09.12/hw09.12/2dArray.cs
+++ hw09.12/hw09.12/2dArray.cs
@@ -1,50 +1,50 @@
-﻿//namespace hw1;
-//class _2dArrays
-//{
-//    static void Main()
-//    {
-//        Console.WriteLine("Enter N: ");
-//        int N = int.Parse(Console.ReadLine());
+﻿namespace hw1;
+class _2dArrays
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter N: ");
+        int N = int.Parse(Console.ReadLine());
 
-//        Console.WriteLine("Enter M: ");
-//        int M = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter M: ");
+        int M = int.Parse(Console.ReadLine());
 
-//        int num = 1;
+        int num = 1;
 
-//        if (N <= 0 || N > 10 || M <= 0 || M > 10)
-//        {
-//            Console.WriteLine("Please enter a number between 1 and 10");
-//            N = int.Parse(Console.ReadLine());
-//            M = int.Parse(Console.ReadLine());
-//        }
+        if (N <= 0 || N > 10 || M <= 0 || M > 10)
+        {
+            Console.WriteLine("Please enter a number between 1 and 10");
+            N = int.Parse(Console.ReadLine());
+            M = int.Parse(Console.ReadLine());
+        }
 
-//        int[,] result = new int[N, M];
+        int[,] result = new int[N, M];
 
-//        for (int i = 0; i < M; i++)
-//        {
-//            if (i % 2 == 0)
-//            {
-//                for (int j = 0; j < N; j++)
-//                {
-//                    result[i, j] = num++;
-//                }
-//            }
-//            else
-//            {
-//                for (int j = N - 1; j >= 0; j--)
-//                {
-//                    result[i, j] = num++;
-//                }
-//            }
-//        }
+        for (int i = 0; i < M; i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    result[i, j] = num++;
+                }
+            }
+            else
+            {
+                for (int j = N - 1; j >= 0; j--)
+                {
+                    result[i, j] = num++;
+                }
+            }
+        }
 
-//        for (int i = 0; i < M; i++)
-//        {
-//            for (int j = 0; j < N; j++)
-//            {
-//                Console.Write("{0, 4}", result[i, j]);
-//            }
-//            Console.WriteLine();
-//        }
-//    }
-//}
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write("{0, 4}", result[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}

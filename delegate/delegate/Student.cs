7369ea3910aee4048083c9b66d322b75f0c8e14d
devitfro <alinabogdanovaaaaa@gmail.com>
2025-01-15 19:08:05 +0200

    update

--- delegate/delegate/Student.cs
+++ delegate/delegate/Student.cs
@@ -1,4 +1,7 @@
-﻿namespace Delegate;
+﻿using System.Reflection;
+using System.Xml.Linq;
+
+namespace Delegate;
 
 class Student
 {
@@ -10,94 +13,107 @@ class Student
 
     private DateTime _birthday;
 
-    private List<int> _gradesCredit = new List<int>();
-    private List<int> _gradesCourseWork = new List<int>();
     private List<int> _gradesExam = new List<int>();
 
     // c-tor
     public Student()
+        : this("Name", "Surname", "Patronymic", DateTime.Now, "undefined", "undefined", new List<int>())
     {
-        SetName("Name");
-        SetSurname("Surname");
-        SetPatronymic("Patronymic");
-        SetBirthday(DateTime.Now);
-        SetAddress("undefined");
-        SetTelNumber("undefined");
     }
 
     public Student(string name, string surname, string patronymic, DateTime birthday, string address, string telNumber)
+        : this(name, surname, patronymic, birthday, address, telNumber, new List<int>())
     {
-        this._name = name;
-        this._surname = surname;
-        this._patronymic = patronymic;
-        this._birthday = birthday;
-        this._address = address;
-        this._telNumber = telNumber;
     }
+
     public Student(string name, string surname, string patronymic, DateTime birthday, string address, string telNumber, List<int> grades)
     {
-        this._name = name;
-        this._surname = surname;
-        this._patronymic = patronymic;
-        this._birthday = birthday;
-        this._address = address;
-        this._telNumber = telNumber;
-        this._gradesExam = new List<int>();
+        SetName(name);
+        SetSurname(surname);
+        SetPatronymic(patronymic);
+        SetBirthday(birthday);
+        SetAddress(address);
+        SetTelNumber(telNumber);
+        SetGradesExam(grades);
     }
 
     // setters
-    public void SetName(string name) =>
+    public void SetName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Name cannot be null or empty.");
+            return;
+        }
         this._name = name;
+    }
+        
+    public void SetSurname(string surname)
+    {
+        if (string.IsNullOrWhiteSpace(surname))
+        {
+            Console.WriteLine("Surname cannot be null or empty.");
+            return;
+        }
 
-    public void SetSurname(string surname) =>
         this._surname = surname;
+    }    
 
-    public void SetPatronymic(string patronymic) =>
+    public void SetPatronymic(string patronymic)
+    {
+        if (string.IsNullOrWhiteSpace(patronymic))
+        {
+            Console.WriteLine("Patronymic cannot be null or empty.");
+            return;
+        }
+
         this._patronymic = patronymic;
+    }
+     
 
-    public void SetAddress(string address) =>
+    public void SetAddress(string address)
+    {
+        if (string.IsNullOrWhiteSpace(address))
+        {
+            Console.WriteLine("Address cannot be null or empty.");
+            return;
+        }
+
         this._address = address;
+    }
+        
 
-    public void SetTelNumber(string telNumber) =>
+    public void SetTelNumber(string telNumber)
+    {
+        if (telNumber.Length < 10 || telNumber.Length > 15)
+        {
+            Console.WriteLine("Telephone number must be between 10 and 15 characters long.");
+            return;
+        }
+
         this._telNumber = telNumber;
+    }
 
-    public void SetBirthday(DateTime birthday) =>
+    public void SetBirthday(DateTime birthday) {
+        DateTime today = DateTime.Now;
+
+        if (birthday > today)
+        {
+            Console.WriteLine("Birthday cannot be in the future.");
+            return;
+        }
+
+        int age = today.Year - birthday.Year;
+
+        if (age < 15 || age > 100)
+        {
+            Console.WriteLine("Maybe not correct..");
+            return;
+        }
+
         this._birthday = birthday;
-
-    public void SetGradesCredit(List<int> grades)
-    {
-        if (grades == null)
-        {
-            grades = new List<int>();
-            Console.WriteLine("Grades list is empty!");
-        }
-
-        foreach (int grade in grades)
-        {
-            if (grade >= 0 && grade <= 100)
-            {
-                _gradesCredit.Add(grade);
-            }
-        }
     }
-
-    public void SetGradeCourseWork(List<int> grades)
-    {
-        if (grades == null)
-        {
-            grades = new List<int>();
-            Console.WriteLine("Grades list is empty!");
-        }
-
-        foreach (int grade in grades)
-        {
-            if (grade >= 0 && grade <= 50)
-            {
-                _gradesCourseWork.Add(grade);
-            }
-        }
-    }
-
+      
     public void SetGradesExam(List<int> grades)
     {
         if (grades == null)
@@ -128,10 +144,6 @@ class Student
 
     public DateTime GetBirthday() => _birthday;
 
-    public List<int> GetGradesCredit() => _gradesCredit;
-
-    public List<int> GetGradesCourseWork() => _gradesCourseWork;
-
     public List<int> GetGradesExam() => _gradesExam;
 
     public override string ToString() =>
@@ -143,11 +155,10 @@ class Student
         $"Address: {_address}\n" +
         $"Phone number: {_telNumber}";
 
+    // methods
     public void ShowStudent()
     {
         Console.WriteLine(ToString());
-        Console.WriteLine("Grades credit: " + string.Join(", ", _gradesCredit));
-        Console.WriteLine("Grades course work: " + string.Join(", ", _gradesCourseWork));
         Console.WriteLine("Grades exam: " + string.Join(", ", _gradesExam));
     }
 }
--- delegate/delegate/obj/Debug/net9.0/delegate.AssemblyInfo.cs
+++ delegate/delegate/obj/Debug/net9.0/delegate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("delegate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea5438570e80f7a6145560ba5345e4e17edf5dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b091adee99b5712dd76a2d356e0bc951234c157")]
 [assembly: System.Reflection.AssemblyProductAttribute("delegate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("delegate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

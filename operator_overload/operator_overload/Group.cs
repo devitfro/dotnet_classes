d6d608561de543bf914c7eefa1c7c04c571da310
devitfro <alinabogdanovaaaaa@gmail.com>
2025-01-21 18:31:59 +0200

    update

---
+++ operator_overload/operator_overload/Group.cs
@@ -0,0 +1,161 @@
+﻿namespace operator_overload;
+
+class Group
+{
+    private List<Student> _students;
+
+    private string _groupName;
+    private string _groupSpecialist;
+    private int _courseNumber;
+
+    // c-tor
+    public Group() : this("Default Group", "Default Specialist", 1) { }
+
+    public Group(string groupName, string groupSpecialist, int courseNumber)
+    {
+        _students = new List<Student>();
+
+        SetGroupName(groupName);
+        SetGroupSpecialist(groupSpecialist);
+        SetCourseNumber(courseNumber);
+    }
+
+    // setters
+    public void SetGroupName(string groupName)
+    {
+        if (string.IsNullOrWhiteSpace(groupName))
+        {
+            Console.WriteLine("Name cannot be null or empty.");
+            return;
+        }
+        _groupName = groupName;
+    }
+
+
+    public void SetGroupSpecialist(string groupSpecialist)
+    {
+        if (string.IsNullOrWhiteSpace(groupSpecialist))
+        {
+            Console.WriteLine("Name cannot be null or empty.");
+            return;
+        }
+        _groupSpecialist = groupSpecialist;
+    }
+
+    public void SetCourseNumber(int courseNumber) =>
+       _courseNumber = courseNumber;
+
+    // getters
+    public string GetGroupName() => _groupName;
+
+    public string GetGroupSpecialist() => _groupSpecialist;
+
+    public int GetCourseNumber() => _courseNumber;
+
+    // methods
+    public void ShowGroup()
+    {
+        Console.WriteLine($"Group: {_groupName}, Specialisation: {_groupSpecialist}, Course: {_courseNumber}");
+
+        Console.WriteLine("Students:");
+
+        _students.Sort((s1, s2) => string.Compare(s1.GetSurname(), s2.GetSurname(), StringComparison.Ordinal));
+
+        for (int i = 0; i < _students.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}.{_students[i].GetSurname()} {_students[i].GetName()}");
+        }
+    }
+
+    public void AddStudent(Student student)
+    {
+        _students.Add(student);
+    }
+
+    public void EditStudentData(Student student, string surname, string addess, string telNumber)
+    {
+        student.SetSurname(surname);
+        student.SetAddress(addess);
+        student.SetTelNumber(telNumber);
+    }
+
+    public void RemoveStudent(List<Student> students)
+    {
+        foreach (Student student in students)
+        {
+            if (!CheckGradeExam(student, student.GetGradesExam()))
+            {
+                students.Remove(student);
+            }
+        }
+    }
+
+    public void RemoveStudentsWithLowGrades()
+    {
+        _students = _students.Where(student => CheckGradeExam(student, student.GetGradesExam())).ToList();
+    }
+
+    public bool CheckGradeExam(Student student, List<int> gradesExam)
+    {
+
+        if (gradesExam == null || gradesExam.Count == 0)
+            return false;
+
+        int gradeSum = 0;
+
+        foreach (int grade in gradesExam)
+        {
+            gradeSum += grade;
+        }
+
+        double avgGrade = gradeSum / gradesExam.Count;
+
+        return avgGrade >= 50;
+    }
+
+    // overload operator
+    public static bool operator true(Group group) => group._students.Count > 0;
+
+    public static bool operator false(Group group) => group._students.Count == 0;
+
+    public static bool operator >(Group group1, Group group2)
+    {
+        if (group2 is null)
+        {
+            throw new ArgumentNullException(nameof(group2));
+        }
+
+        return group1._students.Count > group2._students.Count;
+    }
+
+    public static bool operator <(Group group1, Group group2)
+    {
+        if (group2 is null)
+        {
+            throw new ArgumentNullException(nameof(group2));
+        }
+
+        return group1._students.Count < group2._students.Count;
+    }
+
+    public static bool operator ==(Group group1, Group group2)
+    {
+        if (group2 is null)
+        {
+            throw new ArgumentNullException(nameof(group2));
+        }
+
+        return group1._students.Count == group2._students.Count;
+    }
+
+    public static bool operator !=(Group a, Group b)
+    {
+        return a._students.Count != b._students.Count;
+    }
+    public Student this[int index] 
+    { 
+        get => _students[index]; 
+        set => _students[index] = value; 
+    }
+}
+
---
+++ operator_overload/operator_overload/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace operator_overload;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Student student1 = new Student(
+                "Ки Хун",
+                "Сон",
+                "-",
+                new DateTime(1980, 5, 15),
+                "Сеул",
+                "1234567890",
+                new List<int> { 85, 90, 75 },
+                new List<int> { 45, 36, 48 },
+                new List<int> { 95, 90, 75 }
+            );
+
+        Student student2 = new Student(
+                "Хван",
+                "Ин",
+                "Хо",
+                new DateTime(1989, 3, 22),
+                "Сеул",
+                "0987654321",
+                new List<int> { 90, 83, 100 },
+                new List<int> { 30, 45, 50 },
+                new List<int> { 92, 80, 100 }
+            );
+
+        Student student3 = new Student(
+            "Джо",
+            "Хён-джу",
+            "-",
+            new DateTime(1990, 12, 30),
+            "Сеул",
+            "1122334455",
+            new List<int> { 67, 88, 92 },
+            new List<int> { 45, 48, 50 },
+            new List<int> { 85, 88, 93 }
+        );
+
+        // Вывод данных о студентах
+        Console.WriteLine("Student 1 details:");
+
+        student1.ShowStudent();
+        Console.WriteLine();
+
+        Console.WriteLine("Student 2 details:");
+        student2.ShowStudent();
+        Console.WriteLine();
+
+        Console.WriteLine("Student 3 details:");
+        student3.ShowStudent();
+        Console.WriteLine();
+
+        // Создание группы1 и группы2
+        var group1 = new Group();
+        group1.SetGroupName("SG2");
+        group1.SetGroupSpecialist("Math");
+        group1.SetCourseNumber(2024);
+
+        group1.AddStudent(student1);
+        group1.AddStudent(student2);
+
+        var group2 = new Group();
+        group2.SetGroupName("SG3");
+        group2.SetGroupSpecialist("Biology");
+        group2.SetCourseNumber(2024);
+
+        group2.AddStudent(student3);
+
+        group1.ShowGroup();
+        group2.ShowGroup();
+
+        Console.WriteLine("Comparing Students:");
+        Console.WriteLine(student1 > student2);
+        Console.WriteLine(student1 < student2);
+        Console.WriteLine(student1 == student2);
+        Console.WriteLine(student1 != student2);
+
+        Console.WriteLine("Student1 is " + (student1 ? "trueee" : "falsee")); 
+        Console.WriteLine("Student2 is " + (student2 ? "trueee" : "falsee"));
+
+        Console.WriteLine("Comparing Groups:");
+        Console.WriteLine(group1 > group2);
+        Console.WriteLine(group1 < group2);
+        Console.WriteLine(group1 == group2);
+        Console.WriteLine(group1 != group2);
+
+        Console.WriteLine("Group1 is " + (group1 ? "valid" : "not valid"));
+        Console.WriteLine("Group2 is " + (group2 ? "valid" : "not valid"));
+
+        Console.WriteLine("Group1 first student: " + group1[0].GetName());
+    }
+}
+

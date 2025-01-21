1ec534ab1f09b31fd22562da72f3f9ced49b5db0
devitfro <alinabogdanovaaaaa@gmail.com>
2025-01-21 17:35:32 +0200

    update

---
+++ cycles/hw06.12/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace cycles { 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter count of steps: ");
+
+            string user_steps = Console.ReadLine();
+            bool success = int.TryParse(user_steps, out int steps);
+
+            if (!success || steps <= 0)
+            {
+                Console.WriteLine("Wrong, please enter correct number...");
+                return;
+            }
+
+            int indentation = 3;
+            for (int i = 0; i < steps; i++)
+            {
+                Console.WriteLine("***".PadLeft(indentation));
+                Console.WriteLine("  *".PadLeft(indentation));
+                indentation += 3;
+            }
+            Console.WriteLine("***".PadLeft(indentation));
+        }
+    }
+}
+
+

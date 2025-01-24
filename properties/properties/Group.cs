776ab106f5463b0cc342411db77d35d0601f27ff
devitfro <alinabogdanovaaaaa@gmail.com>
2025-01-24 20:46:59 +0200

    update

---
+++ properties/properties/Group.cs
@@ -0,0 +1,213 @@
+﻿namespace properties;
+
+class Group
+{
+    private List<Student> _students;
+
+    private string _groupName;
+    private string _groupSpecialist;
+    private int _courseNumber;
+
+    // c-tor
+    public Group() : this("Default Group", "Default Specialist", 1) { }
+
+    //public Group(string groupName, string groupSpecialist, int courseNumber)
+    //{
+    //    _students = new List<Student>();
+
+    //    SetGroupName(groupName);
+    //    SetGroupSpecialist(groupSpecialist);
+    //    SetCourseNumber(courseNumber);
+    //}
+
+    // properties
+    // new c-tor with properties
+    public Group(string groupName, string groupSpecialist, int courseNumber)
+    {
+        _students = new List<Student>();
+
+        GroupName = groupName;
+        GroupSpecialist = groupSpecialist;
+        CourseNumber = courseNumber;
+    }
+
+    public string GroupName
+    {
+        get => _groupName;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                Console.WriteLine("Group name cannot be null or empty.");
+                return;
+            }
+            _groupName = value;
+        }
+    }
+
+    public string GroupSpecialist
+    {
+        get => _groupSpecialist;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                Console.WriteLine("Specialisation cannot be null or empty.");
+                return;
+            }
+            _groupSpecialist = value;
+        }
+    }
+
+    public int CourseNumber
+    {
+        get => _courseNumber;
+        set
+        {
+            if (value < 1 || value > 5)
+            {
+                Console.WriteLine("Course number must be between 1 and 5.");
+                return;
+            }
+            _courseNumber = value;
+        }
+    }
+
+    // setters
+    public void SetGroupName(string groupName)
+    {
+        if (string.IsNullOrWhiteSpace(groupName))
+        {
+            Console.WriteLine("Name cannot be null or empty.");
+            return;
+        }
+        _groupName = groupName;
+    }
+
+
+    public void SetGroupSpecialist(string groupSpecialist)
+    {
+        if (string.IsNullOrWhiteSpace(groupSpecialist))
+        {
+            Console.WriteLine("Name cannot be null or empty.");
+            return;
+        }
+        _groupSpecialist = groupSpecialist;
+    }
+
+    public void SetCourseNumber(int courseNumber) =>
+       _courseNumber = courseNumber;
+
+    // getters
+    public string GetGroupName() => _groupName;
+
+    public string GetGroupSpecialist() => _groupSpecialist;
+
+    public int GetCourseNumber() => _courseNumber;
+
+    // methods
+    public void ShowGroup()
+    {
+        Console.WriteLine($"Group: {_groupName}, Specialisation: {_groupSpecialist}, Course: {_courseNumber}");
+
+        Console.WriteLine("Students:");
+
+        _students.Sort((s1, s2) => string.Compare(s1.GetSurname(), s2.GetSurname(), StringComparison.Ordinal));
+
+        for (int i = 0; i < _students.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}.{_students[i].GetSurname()} {_students[i].GetName()}");
+        }
+    }
+
+    public void AddStudent(Student student)
+    {
+        _students.Add(student);
+    }
+
+    public void EditStudentData(Student student, string surname, string addess, string telNumber)
+    {
+        student.SetSurname(surname);
+        student.SetAddress(addess);
+        student.SetTelNumber(telNumber);
+    }
+
+    public void RemoveStudent(List<Student> students)
+    {
+        foreach (Student student in students)
+        {
+            if (!CheckGradeExam(student, student.GetGradesExam()))
+            {
+                students.Remove(student);
+            }
+        }
+    }
+
+    public void RemoveStudentsWithLowGrades()
+    {
+        _students = _students.Where(student => CheckGradeExam(student, student.GetGradesExam())).ToList();
+    }
+
+    public bool CheckGradeExam(Student student, List<int> gradesExam)
+    {
+        if (gradesExam == null || gradesExam.Count == 0)
+            return false;
+
+        int gradeSum = 0;
+
+        foreach (int grade in gradesExam)
+        {
+            gradeSum += grade;
+        }
+
+        double avgGrade = gradeSum / gradesExam.Count;
+
+        return avgGrade >= 70;
+    }
+
+    // overload operator
+    public static bool operator true(Group group) => group._students.Count > 0;
+
+    public static bool operator false(Group group) => group._students.Count == 0;
+
+    public static bool operator >(Group group1, Group group2)
+    {
+        if (group2 is null)
+        {
+            throw new ArgumentNullException(nameof(group2));
+        }
+
+        return group1._students.Count > group2._students.Count;
+    }
+
+    public static bool operator <(Group group1, Group group2)
+    {
+        if (group2 is null)
+        {
+            throw new ArgumentNullException(nameof(group2));
+        }
+
+        return group1._students.Count < group2._students.Count;
+    }
+
+    public static bool operator ==(Group group1, Group group2)
+    {
+        if (group2 is null)
+        {
+            throw new ArgumentNullException(nameof(group2));
+        }
+
+        return group1._students.Count == group2._students.Count;
+    }
+
+    public static bool operator !=(Group group1, Group group2)
+    {
+        return group1._students.Count != group2._students.Count;
+    }
+
+    public Student this[int index]
+    {
+        get => _students[index];
+        set => _students[index] = value;
+    }
+}
---
+++ properties/properties/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace properties;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Group group = new Group();
+            group.GroupName = "";
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+        try
+        {
+            Student student = new Student();
+            student.Name = null;
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}

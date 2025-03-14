713b96ed4a6310b9ee46026cdd946ad67242de72
devitfro <alinabogdanovaaaaa@gmail.com>
2025-03-14 19:25:59 +0200

    update

---
+++ lyambda/lyambda/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lyambda
+{
+    delegate bool StudentFilter(Student student);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group = new Group("CS-101", "Computer Science", 2, new List<Student>
+            {
+                new Student("Ки Хун", "Сон", "Сон", new DateTime(2002, 5, 12), "NY", "1234567890", new List<int>{ 90, 85, 88 }, new List<int>{ 45, 48 }, new List<int>{ 80, 90 }),
+                new Student("Хван", "Ин", "Хо", new DateTime(2003, 6, 25), "Сеул", "0987654321", new List<int>{ 70, 75, 78 }, new List<int>{ 40, 42 }, new List<int>{ 60, 65 }),
+                new Student("Charlie", "Brown", "Sergeevich", new DateTime(2001, 2, 20), "TX", "1122334455", new List<int>{ 95, 92, 93 }, new List<int>{ 50, 49 }, new List<int>{ 98, 100 })
+            });
+
+            // Фильтры на анонимных методах
+            var excellentStudents = group.FilterStudents(delegate (Student s) { return s.GetAverageGrade() >= 10; });
+            var nameStartsWithA = group.FilterStudents(delegate (Student s) { return s.Name.StartsWith("A"); });
+            var hasGrade2 = group.FilterStudents(delegate (Student s) { return s._gradesExam.Contains(2); });
+
+            // Фильтры на лямбда-выражениях
+            var averageGrade = group.Students.Average(s => s.GetAverageGrade());
+            var topStudents = group.FilterStudents(s => s.GetAverageGrade() > averageGrade);
+            var longNameStudents = group.FilterStudents(s => s.Name.Length > 5);
+            var evenGradeCountStudents = group.FilterStudents(s => s._gradesExam.Count % 2 == 0);
+            var highTotalGradeStudents = group.FilterStudents(s => s._gradesExam.Sum() + s._gradesExam.Sum() > 50);
+
+            // Вывод результатов
+            PrintStudents("Отличники:", excellentStudents);
+            PrintStudents("Имена начинаются с 'А':", nameStartsWithA);
+            PrintStudents("Есть оценка 2 за экзамен:", hasGrade2);
+            PrintStudents("Средний балл выше среднего:", topStudents);
+            PrintStudents("Имя длиннее 5 символов:", longNameStudents);
+            PrintStudents("Четное количество оценок:", evenGradeCountStudents);
+            PrintStudents("Сумма всех оценок больше 50:", highTotalGradeStudents);
+        }
+
+        static void PrintStudents(string message, List<Student> students)
+        {
+            Console.WriteLine($"\n{message}");
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+    }
+}
+
+
+
---
+++ lyambda/lyambda/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("lyambda")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lyambda")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("271dc7ce-bf64-483b-894f-a32db8713c05")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

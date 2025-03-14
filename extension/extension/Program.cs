209417567a4427a2c87e610e7a2327400b1c8396
devitfro <alinabogdanovaaaaa@gmail.com>
2025-03-14 19:29:27 +0200

    update

---
+++ extension/extension/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extension
+{
+    public static class Extensions
+    {
+        public static void Print<T>(this T value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            "Hello world".Print();
+            2024.Print();
+            0.08.Print();
+        }
+    }
+}
+
+
+
+
+
